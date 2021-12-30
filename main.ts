1be81206a06d58152240a3a0157a00cababb2345
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2021-12-30 17:33:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,22 +4,20 @@ radio.onReceivedValue(function (name, value) {
     } else if (name == "B") {
         angoloS0 += 5
     } else if (name == "R") {
-        speedM1 += -10
+        speedM1 += -50
     } else if (name == "L") {
-        speedM1 += 10
+        speedM1 += 50
     }
     if (angoloS0 > 360) {
         angoloS0 = 360
     } else if (angoloS0 < 0) {
         angoloS0 = 0
     }
-    if (speedM1 > 100) {
-        speedM1 = 100
-    } else if (speedM1 < -100) {
-        speedM1 = -100
-    }
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, speedM1)
+    basic.pause(50)
+    wuKong.stopMotor(wuKong.MotorList.M1)
+    speedM1 = 0
 })
 let speedM1 = 0
 let angoloS0 = 0
