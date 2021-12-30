1438b3f8f28c519c16f4c39171e2aedad5e0852b
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2021-12-30 16:16:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,25 +4,50 @@ radio.onReceivedValue(function (name, value) {
     } else if (name == "B") {
         angoloS0 += 5
     } else if (name == "R") {
-    	
+        speedM1 += -10
     } else if (name == "L") {
-    	
+        speedM1 += 10
     }
     if (angoloS0 > 360) {
         angoloS0 = 360
     } else if (angoloS0 < 0) {
         angoloS0 = 0
     }
+    if (speedM1 > 100) {
+        speedM1 = 100
+    } else if (speedM1 < -100) {
+        speedM1 = -100
+    }
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
-    led.plotBarGraph(
-    angoloS0,
-    1000
-    )
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, speedM1)
 })
+let speedM1 = 0
 let angoloS0 = 0
 radio.setGroup(8)
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 wuKong.setLightMode(wuKong.LightMode.BREATH)
 let countRadio = 0
 angoloS0 = 0
+speedM1 = 0
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
+wuKong.stopMotor(wuKong.MotorList.M1)
+basic.forever(function () {
+    if (angoloS0 == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (angoloS0 > 360) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
