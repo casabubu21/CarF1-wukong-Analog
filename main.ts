550dd3d6017994fc8596067b30b01a034b941405
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2021-12-30 15:50:46 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "F") {
-    	
-    } else if (name == "B") {
-    	
-    } else if (name == "R") {
-        angoloS0 += 5
-    } else if (name == "L") {
         angoloS0 += -5
+    } else if (name == "B") {
+        angoloS0 += 5
+    } else if (name == "R") {
+    	
+    } else if (name == "L") {
+    	
     }
     if (angoloS0 > 360) {
+        angoloS0 = 360
+    } else if (angoloS0 < 0) {
         angoloS0 = 0
     }
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
@@ -20,7 +22,6 @@ radio.onReceivedValue(function (name, value) {
 let angoloS0 = 0
 radio.setGroup(8)
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
-wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 0)
 wuKong.setLightMode(wuKong.LightMode.BREATH)
 let countRadio = 0
 angoloS0 = 0
