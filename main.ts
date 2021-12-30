db74970f233e7d18a7c813c854260a2d5b0485b5
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2021-12-30 13:10:42 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,11 +8,20 @@ radio.onReceivedValue(function (name, value) {
     } else if (name == "L") {
         angoloS0 += -5
     }
-    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, angoloS0)
+    if (angoloS0 > 360) {
+        angoloS0 = 0
+    }
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
+    led.plotBarGraph(
+    angoloS0,
+    1000
+    )
 })
 let angoloS0 = 0
 radio.setGroup(8)
-wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 0)
-wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S1, 0)
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 0)
 wuKong.setLightMode(wuKong.LightMode.BREATH)
+let countRadio = 0
 angoloS0 = 0
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
