8c9fbff026fb0a610b3bfe9e980eed02675381a0
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2022-08-13 18:41:22 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,25 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "F") {
-        angoloS0 += -5
+        speedM1 += -1
     } else if (name == "B") {
-        angoloS0 += 5
+        speedM1 += 1
     } else if (name == "R") {
-        speedM1 += -50
+        angoloS0 += -2
     } else if (name == "L") {
-        speedM1 += 50
+        angoloS0 += 2
     }
-    if (angoloS0 > 360) {
-        angoloS0 = 360
-    } else if (angoloS0 < 0) {
-        angoloS0 = 0
+    if (angoloS0 > 30) {
+        angoloS0 = 30
+    } else if (angoloS0 < -30) {
+        angoloS0 = -30
     }
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, speedM1)
     basic.pause(50)
-    wuKong.stopMotor(wuKong.MotorList.M1)
-    speedM1 = 0
 })
 let speedM1 = 0
 let angoloS0 = 0
 radio.setGroup(8)
-wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 wuKong.setLightMode(wuKong.LightMode.BREATH)
 let countRadio = 0
 angoloS0 = 0
