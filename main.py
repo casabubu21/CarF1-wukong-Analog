1e7e94eb0125c87e0594307be6af817475df4856
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2022-08-18 16:07:38 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 def on_received_value(name, value):
-    global angoloS0, speedM1
+    global speedM1, angoloS0
     if name == "F":
-        angoloS0 += -5
+        speedM1 += -6
     elif name == "B":
-        angoloS0 += 5
-    elif name == "R":
-        speedM1 += -50
+        speedM1 += 6
     elif name == "L":
-        speedM1 += 50
-    if angoloS0 > 360:
-        angoloS0 = 360
-    elif angoloS0 < 0:
-        angoloS0 = 0
+        angoloS0 += -6
+    elif name == "R":
+        angoloS0 += 6
+    elif name == "S":
+        speedM1 = 0
+        wuKong.stop_motor(wuKong.MotorList.M1)
+    if angoloS0 > 210:
+        angoloS0 = 210
+    elif angoloS0 < 150:
+        angoloS0 = 150
+    if speedM1 >= 100:
+        speedM1 = 100
+    elif speedM1 <= -100:
+        speedM1 = -100
     wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
     wuKong.set_motor_speed(wuKong.MotorList.M1, speedM1)
     basic.pause(50)
-    wuKong.stop_motor(wuKong.MotorList.M1)
-    speedM1 = 0
 radio.on_received_value(on_received_value)
 
 speedM1 = 0
 angoloS0 = 0
 radio.set_group(8)
-wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 wuKong.set_light_mode(wuKong.LightMode.BREATH)
 countRadio = 0
-angoloS0 = 0
+angoloS0 = 180
 speedM1 = 0
 wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
 wuKong.stop_motor(wuKong.MotorList.M1)
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "F") {
-        speedM1 += -6
-    } else if (name == "B") {
         speedM1 += 6
+    } else if (name == "B") {
+        speedM1 += -6
     } else if (name == "L") {
         angoloS0 += -6
     } else if (name == "R") {
@@ -21,10 +21,19 @@ radio.onReceivedValue(function (name, value) {
     } else if (speedM1 <= -100) {
         speedM1 = -100
     }
+    if (piopio == false) {
+        piopio = true
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else {
+        piopio = false
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, speedM1)
     basic.pause(50)
 })
+let strip: neopixel.Strip = null
+let piopio = false
 let speedM1 = 0
 let angoloS0 = 0
 radio.setGroup(8)
@@ -32,5 +41,8 @@ wuKong.setLightMode(wuKong.LightMode.BREATH)
 let countRadio = 0
 angoloS0 = 180
 speedM1 = 0
+piopio = false
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
 wuKong.stopMotor(wuKong.MotorList.M1)
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.showColor(neopixel.rgb(155, 10, 255))
