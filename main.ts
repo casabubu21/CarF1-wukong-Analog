9cfbb23a3cc729d6b4343869857268e56fdc5ca1
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2022-11-22 15:40:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,20 +23,10 @@ radio.onReceivedValue(function (name, value) {
     } else if (speedM1 <= -100) {
         speedM1 = -100
     }
-    if (piopio == 0) {
-        piopio = 1
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    } else {
-        piopio = 0
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    }
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, speedM1)
     basic.pause(50)
 })
-let bip = 0
-let strip: neopixel.Strip = null
-let piopio = 0
 let speedM1 = 0
 let angoloS0 = 0
 let countRadio = 0
@@ -44,16 +34,7 @@ radio.setGroup(8)
 wuKong.setLightMode(wuKong.LightMode.BREATH)
 angoloS0 = 180
 speedM1 = 0
-piopio = 0
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angoloS0)
 wuKong.stopMotor(wuKong.MotorList.M1)
-strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.showColor(neopixel.rgb(155, 10, 255))
-basic.forever(function () {
-    if (bip > 1) {
-        music.playTone(988, music.beat(BeatFraction.Whole))
-        basic.pause(350)
-        bip = 0
-    }
-    basic.pause(100)
-})
